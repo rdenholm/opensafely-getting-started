24bae40d0cc09e04fe22ceb7e27f9d9408c57220
rdenholm <69469265+rdenholm@users.noreply.github.com>
2024-11-26 11:42:15 +0000

    Initialize Jongmassey/handle-empty-rstudio-prefs

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail 
+
+/usr/local/bin/pip3 install --user -r .devcontainer/requirements.in
+
+#set R working directory
+! grep -q "$1" $R_HOME/etc/Rprofile.site && sudo tee -a $R_HOME/etc/Rprofile.site <<< "setwd(\"$1\")"
+#set RStudio working directory
+if [ ! -s ~/.config/rstudio/rstudio-prefs.json ]; then nullinput="-n"; fi;
+! grep -q "$1" ~/.config/rstudio/rstudio-prefs.json && cat ~/.config/rstudio/rstudio-prefs.json | jq "$nullinput" ". + {\"initial_working_directory\":\"$1\"}" >  ~/.config/rstudio/rstudio-prefs.json 
+
+#download and extract latest ehrql source
+wget https://github.com/opensafely-core/ehrql/archive/main.zip -P .devcontainer
+unzip -o .devcontainer/main.zip -d .devcontainer/
+rm .devcontainer/main.zip
